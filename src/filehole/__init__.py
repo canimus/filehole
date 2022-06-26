b936ba448dfd8e21f685345b56ed60b9bcf27837
Herminio Vazquez <hvazquez@Herminios-Mac-mini.local>
2022-06-26 17:18:38 +0200

    Addition of the parts in file

--- src/filehole/__init__.py
+++ src/filehole/__init__.py
@@ -3,10 +3,11 @@ import pandas as pd
 from typing import Protocol, List
 from toolz import compose
 from operator import attrgetter as at
+from operator import itemgetter as it
 from operator import methodcaller as mc
 from pathlib import Path
 from datetime import datetime, timedelta, date
-from functools import partial
+from itertools import chain
 
 _iso_format = mc("strftime", "%Y-%m-%d")
 
@@ -21,20 +22,22 @@ __all__ = ["filehole"]
 
 def filehole(
     fs: Globable,
-    file_pattern: str,
-    date_pattern: str = "(\d{4}-\d{2}-\d{2})",
-    start_schedule: str = _iso_format(date(datetime.today().year, 1, 1)),
-    end_schedule: str = _iso_format(datetime.today()),
-    frequency: str = "D"
+    pattern_file: str,
+    pattern_date: str = "(\d{4}-\d{2}-\d{2})",
+    pattern_cast: str = "%Y-%m-%d",
+    schedule_start: str = _iso_format(date(datetime.today().year, 1, 1)),
+    schedule_end: str = _iso_format(datetime.today()),
+    schedule_frequency: str = "D",
+    holidays_excluded: bool = False,
+    holidays_country: str = "NL",
 ):
     """Search for file delivery holes in a schedule"""
-    files = fs.glob(file_pattern)
-    expr = re.compile(date_pattern)
-    _has_date = compose(at("parts"), Path)
-    return pd.DataFrame(
-        zip(
-            files,
-            [any(map(expr.match, file_name)) for file_name in map(_has_date, files)],
-        ),
-        columns=["file", "found"],
+    _files = fs.glob(pattern_file)
+    _expr = re.compile(pattern_date)
+    _file_parts = compose(at("parts"), Path)
+    _cast_date = lambda a: datetime.strptime(a, pattern_cast)
+    _match_composition = compose(
+        _iso_format, _cast_date, it(0), mc("groups"), _expr.match
     )
+
+    file_matches = [any(map(_expr.match, parts)) for parts in list(map(_file_parts, _files))]
